c32b899c26bd48a02419cabb490b8f857bd3b3cd
Samuel Ray <raysj@vcu.edu>
2026-03-24 23:40:29 -0400

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Samuel Ray on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by Samuel Ray on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
